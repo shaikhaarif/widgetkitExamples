b218774d584043016b46399de191bdbb465ea1bc
shaikhaarif <aarifsk1618@gmail.com>
2023-01-16 18:51:15 +0900

    dummy widget

---
+++ calendarWidget/calendarWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  calendarWidgetBundle.swift
+//  calendarWidget
+//
+//  Created by Aarif Shaikh on 2023/01/16.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct calendarWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        calendarWidget()
+    }
+}
