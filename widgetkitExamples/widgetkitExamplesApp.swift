f7c660f7413f427d22e21ad421e4d3329e5f4ba5
shaikhaarif <aarifsk1618@gmail.com>
2023-01-16 18:32:36 +0900

    Initial Commit

---
+++ widgetkitExamples/widgetkitExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetkitExamplesApp.swift
+//  widgetkitExamples
+//
+//  Created by Aarif Shaikh on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct widgetkitExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
